94b76177b2e525fff457b919b84d6f16de1ea007
dominic <dominic@edu174228.fhi-fokus.de>
2019-02-12 08:47:23 +0100

    My first test to see if it works with Eclipse

---
+++ src/helloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author cout-DOMINIC
+ *
+ */
+package helloWorld;
+
+class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}
+}
