afa3a3c0f720b136d496df5f50d3a759897dc9ea
dominic <dominic@edu174228.fhi-fokus.de>
2019-02-12 09:04:25 +0100

    My first test to see if it works with Eclipse

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -11,7 +11,7 @@ class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World. This is a GitHub test number 3.");
+		System.out.println("Hello World. This is a GitHub test.");
 
 	}
 }
